328eb055ee6969d5aa422ea291a82f7be4071969
Syed-Talmin <saiyedsufyan7@gmail.com>
2025-09-07 16:04:15 +0530

    __dirname

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,6 @@ import cookieParser from "cookie-parser";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// __dirname banane ke liye ES module ka alternative
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -14,14 +13,14 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-// Public folder serve karne ke liye
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/auth", authRouter);
 app.use("/chat", chatRouter);
 app.use("/message", messageRouter);
 
-// React/Frontend ke liye catch-all route
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
